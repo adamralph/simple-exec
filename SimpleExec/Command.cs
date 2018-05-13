9e72d89f9ff6e48a5e74b3a6e775098e73b77732
Paul Knopf <pauldotknopf@gmail.com>
2018-05-13 19:51:21 -0400

    Added a command to capture standard output from a command.

--- SimpleExec/Command.cs
+++ SimpleExec/Command.cs
@@ -38,7 +38,43 @@ namespace SimpleExec
             }
         }
 
-        private static ProcessStartInfo CreateProcessInfo(string name, string args, string workingDirectory) =>
+        public static string Read(string name, string args) => Read(name, args, "");
+
+        public static Task<string> ReadAsync(string name, string args) => ReadAsync(name, args, "");
+
+        public static string Read(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, true);
+                process.Run();
+
+                if (process.ExitCode != 0)
+                {
+                    process.Throw();
+                }
+
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+
+        public static async Task<string> ReadAsync(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, true);
+                await process.RunAsync();
+
+                if (process.ExitCode != 0)
+                {
+                    await process.ThrowAsync();
+                }
+
+                return await process.StandardOutput.ReadToEndAsync();
+            }
+        }
+
+        private static ProcessStartInfo CreateProcessInfo(string name, string args, string workingDirectory, bool captureOutput = false) =>
             new ProcessStartInfo
             {
                 FileName = name,
@@ -46,7 +82,9 @@ namespace SimpleExec
                 WorkingDirectory = workingDirectory,
                 UseShellExecute = false,
                 RedirectStandardError = true,
+                RedirectStandardOutput = captureOutput
             };
+
 
         private static void Run(this Process process)
         {
