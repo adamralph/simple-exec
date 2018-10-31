f6e82feffa749246036c1f4da91264efa1ac7120
joncloud <jdberube@gmail.com>
2018-10-30 19:04:33 -0700

    Add more overloads to handle cancellation tokens

--- SimpleExec/Command-overloads.cs
+++ SimpleExec/Command-overloads.cs
@@ -1,5 +1,6 @@
 namespace SimpleExec
 {
+    using System.Threading;
     using System.Threading.Tasks;
 
     public static partial class Command
@@ -12,9 +13,18 @@ namespace SimpleExec
 
         public static Task RunAsync(string name, string args) => RunAsync(name, args, "", false);
 
+        public static Task RunAsync(string name, string args, CancellationToken cancellationToken) =>
+            RunAsync(name, args, "", false, cancellationToken);
+
         public static Task RunAsync(string name, string args, bool noEcho) => RunAsync(name, args, "", noEcho);
 
+        public static Task RunAsync(string name, string args, bool noEcho, CancellationToken cancellationToken) =>
+            RunAsync(name, args, "", noEcho, cancellationToken);
+
         public static Task RunAsync(string name, string args, string workingDirectory) => RunAsync(name, args, workingDirectory, false);
+
+        public static Task RunAsync(string name, string args, string workingDirectory, CancellationToken cancellationToken) =>
+            RunAsync(name, args, workingDirectory, false, cancellationToken);
 
         public static Task RunAsync(string name, string args, string workingDirectory, bool noEcho) =>
             RunAsync(name, args, workingDirectory, noEcho, default);
@@ -27,9 +37,18 @@ namespace SimpleExec
 
         public static Task<string> ReadAsync(string name, string args) => ReadAsync(name, args, "", false);
 
+        public static Task<string> ReadAsync(string name, string args, CancellationToken cancellationToken) =>
+            ReadAsync(name, args, "", false, cancellationToken);
+
         public static Task<string> ReadAsync(string name, string args, bool noEcho) => ReadAsync(name, args, "", noEcho);
 
+        public static Task<string> ReadAsync(string name, string args, bool noEcho, CancellationToken cancellationToken) =>
+            ReadAsync(name, args, "", noEcho, cancellationToken);
+
         public static Task<string> ReadAsync(string name, string args, string workingDirectory) => ReadAsync(name, args, workingDirectory, false);
+
+        public static Task<string> ReadAsync(string name, string args, string workingDirectory, CancellationToken cancellationToken) =>
+            ReadAsync(name, args, workingDirectory, false, cancellationToken);
 
         public static Task<string> ReadAsync(string name, string args, string workingDirectory, bool noEcho) =>
             ReadAsync(name, args, workingDirectory, noEcho, default);
