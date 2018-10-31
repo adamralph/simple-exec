6adb075c0e20a167f19aad20c93639356e927f87
joncloud <jdberube@gmail.com>
2018-10-30 19:13:01 -0700

    Removes timing test altogether

--- SimpleExecTests/CancellingCommands.cs
+++ SimpleExecTests/CancellingCommands.cs
@@ -13,25 +13,19 @@ namespace SimpleExecTests
         [Fact]
         public async Task CancellingARunningCommandStopsCommand()
         {
-            var watch = Stopwatch.StartNew();
             using (var cancellationTokenSource = new CancellationTokenSource(50))
             {
                 await Command.RunAsync("dotnet", $"exec {Tester.Path} sleep", "", false, cancellationTokenSource.Token);
             }
-            watch.Stop();
-            Assert.True(watch.Elapsed < TimeSpan.FromMilliseconds(1000), $"Command finished outside window in {watch.Elapsed}");
         }
 
         [Fact]
         public async Task CancellingAReadingCommandStopsCommand()
         {
-            var watch = Stopwatch.StartNew();
             using (var cancellationTokenSource = new CancellationTokenSource(50))
             {
                 var result = await Command.ReadAsync("dotnet", $"exec {Tester.Path} sleep", "", false, cancellationTokenSource.Token);
             }
-            watch.Stop();
-            Assert.True(watch.Elapsed < TimeSpan.FromMilliseconds(1000), $"Command finished outside window in {watch.Elapsed}");
         }
     }
 }
