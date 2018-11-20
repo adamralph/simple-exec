b4f7a86989d802aad99a8b6c495225b727ad5691
Adam Ralph <adam@adamralph.com>
2018-11-20 17:56:36 +0100

    add API documentation

--- SimpleExec/Command.cs
+++ SimpleExec/Command.cs
@@ -3,8 +3,24 @@ namespace SimpleExec
     using System.Diagnostics;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Contains methods for running commands and reading standard output (stdout).
+    /// </summary>
     public static partial class Command
     {
+        /// <summary>
+        /// Runs a command.
+        /// By default, the command line is echoed to standard error (stderr).
+        /// </summary>
+        /// <param name="name">The name of the command. This can be a path to an executable file.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory in which to run the command.</param>
+        /// <param name="noEcho">Whether or not to echo the resulting command line and working directory (if specified) to standard error (stderr).</param>
+        /// <exception cref="System.Exception">The command exited with non-zero exit code.</exception>
+        /// <remarks>
+        /// By default, the resulting command line and the working directory (if specified) are echoed to standard error (stderr).
+        /// To suppress this behavior, provide the <paramref name="noEcho"/> parameter with a value of <c>true</c>.
+        /// </remarks>
         public static void Run(string name, string args = null, string workingDirectory = null, bool noEcho = false)
         {
             using (var process = new Process())
@@ -19,6 +35,20 @@ namespace SimpleExec
             }
         }
 
+        /// <summary>
+        /// Runs a command asynchronously.
+        /// By default, the command line is echoed to standard error (stderr).
+        /// </summary>
+        /// <param name="name">The name of the command. This can be a path to an executable file.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory in which to run the command.</param>
+        /// <param name="noEcho">Whether or not to echo the resulting command line and working directory (if specified) to standard error (stderr).</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous running of the command.</returns>
+        /// <exception cref="System.Exception">The command exited with non-zero exit code.</exception>
+        /// <remarks>
+        /// By default, the resulting command line and the working directory (if specified) are echoed to standard error (stderr).
+        /// To suppress this behavior, provide the <paramref name="noEcho"/> parameter with a value of <c>true</c>.
+        /// </remarks>
         public static async Task RunAsync(string name, string args = null, string workingDirectory = null, bool noEcho = false)
         {
             using (var process = new Process())
@@ -33,6 +63,20 @@ namespace SimpleExec
             }
         }
 
+        /// <summary>
+        /// Runs a command and reads standard output (stdout).
+        /// By default, the command line is echoed to standard error (stderr).
+        /// </summary>
+        /// <param name="name">The name of the command. This can be a path to an executable file.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory in which to run the command.</param>
+        /// <param name="noEcho">Whether or not to echo the resulting command line and working directory (if specified) to standard error (stderr).</param>
+        /// <returns>A <see cref="string"/> representing the contents of standard output (stdout).</returns>
+        /// <exception cref="System.Exception">The command exited with non-zero exit code.</exception>
+        /// <remarks>
+        /// By default, the resulting command line and the working directory (if specified) are echoed to standard error (stderr).
+        /// To suppress this behavior, provide the <paramref name="noEcho"/> parameter with a value of <c>true</c>.
+        /// </remarks>
         public static string Read(string name, string args = null, string workingDirectory = null, bool noEcho = false)
         {
             using (var process = new Process())
@@ -49,6 +93,23 @@ namespace SimpleExec
             }
         }
 
+        /// <summary>
+        /// Runs a command and reads standard output (stdout).
+        /// By default, the command line is echoed to standard error (stderr).
+        /// </summary>
+        /// <param name="name">The name of the command. This can be a path to an executable file.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory in which to run the command.</param>
+        /// <param name="noEcho">Whether or not to echo the resulting command line and working directory (if specified) to standard error (stderr).</param>
+        /// <returns>
+        /// A <see cref="Task{string}"/> representing the asynchronous running of the command and reading of standard output (stdout).
+        /// The task result contains the contents of standard output (stdout).
+        /// </returns>
+        /// <exception cref="System.Exception">The command exited with non-zero exit code.</exception>
+        /// <remarks>
+        /// By default, the resulting command line and the working directory (if specified) are echoed to standard error (stderr).
+        /// To suppress this behavior, provide the <paramref name="noEcho"/> parameter with a value of <c>true</c>.
+        /// </remarks>
         public static async Task<string> ReadAsync(string name, string args = null, string workingDirectory = null, bool noEcho = false)
         {
             using (var process = new Process())
