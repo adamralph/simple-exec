e9c4bf08fc25be8200132edd5f90fcd068f190f2
Adam Ralph <adam@adamralph.com>
2020-11-11 13:22:18 +0100

    fix build-time targetting

--- SimpleExecTests/Infra/Tester.cs
+++ SimpleExecTests/Infra/Tester.cs
@@ -3,9 +3,16 @@ namespace SimpleExecTests.Infra
     internal static class Tester
     {
         public static string Path =>
-#if DEBUG
+#if NETCOREAPP2_1 && DEBUG
+            $"../../../../SimpleExecTester/bin/Debug/netcoreapp2.1/SimpleExecTester.dll";
+#endif
+#if NETCOREAPP2_1 && RELEASE
+            $"../../../../SimpleExecTester/bin/Release/netcoreapp2.1/SimpleExecTester.dll";
+#endif
+#if NETCOREAPP3_1 && DEBUG
             $"../../../../SimpleExecTester/bin/Debug/netcoreapp3.1/SimpleExecTester.dll";
-#else
+#endif
+#if NETCOREAPP3_1 && RELEASE
             $"../../../../SimpleExecTester/bin/Release/netcoreapp3.1/SimpleExecTester.dll";
 #endif
     }
