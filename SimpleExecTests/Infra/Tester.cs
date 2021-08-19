704e73cf36521d6e3dd36a22df683d71ef70e35d
Adam Ralph <adam@adamralph.com>
2021-08-19 22:28:42 +0200

    stop testing on .NET Core 2.1

--- SimpleExecTests/Infra/Tester.cs
+++ SimpleExecTests/Infra/Tester.cs
@@ -3,12 +3,6 @@ namespace SimpleExecTests.Infra
     internal static class Tester
     {
         public static string Path =>
-#if NETCOREAPP2_1 && DEBUG
-            $"../../../../SimpleExecTester/bin/Debug/netcoreapp2.1/SimpleExecTester.dll";
-#endif
-#if NETCOREAPP2_1 && RELEASE
-            $"../../../../SimpleExecTester/bin/Release/netcoreapp2.1/SimpleExecTester.dll";
-#endif
 #if NETCOREAPP3_1 && DEBUG
             $"../../../../SimpleExecTester/bin/Debug/netcoreapp3.1/SimpleExecTester.dll";
 #endif
