3b97bd8a7606d032eb174890d43f63ef493329c8
Adam Ralph <adam@adamralph.com>
2022-05-15 09:02:55 +0200

    update SDKs

--- SimpleExecTests/Infra/Capture.cs
+++ SimpleExecTests/Infra/Capture.cs
@@ -5,7 +5,7 @@ namespace SimpleExecTests.Infra
 {
     internal static class Capture
     {
-#if NET5_0_OR_GREATER
+#if NET6_0_OR_GREATER
         private static readonly Lazy<TextWriter> @out = new(() => new StringWriter());
 #else
         private static readonly Lazy<TextWriter> @out = new Lazy<TextWriter>(() => new StringWriter());
--- SimpleExecTests/Infra/Tester.cs
+++ SimpleExecTests/Infra/Tester.cs
@@ -9,12 +9,6 @@ namespace SimpleExecTests.Infra
 #if NETCOREAPP3_1 && RELEASE
             "../../../../SimpleExecTester/bin/Release/netcoreapp3.1/SimpleExecTester.dll";
 #endif
-#if NET5_0 && DEBUG
-            "../../../../SimpleExecTester/bin/Debug/net5.0/SimpleExecTester.dll";
-#endif
-#if NET5_0 && RELEASE
-            "../../../../SimpleExecTester/bin/Release/net5.0/SimpleExecTester.dll";
-#endif
 #if NET6_0 && DEBUG
             "../../../../SimpleExecTester/bin/Debug/net6.0/SimpleExecTester.dll";
 #endif
