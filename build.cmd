fd358fc08be10f44625840aa7971839b86127de4
Adam Ralph <adam@adamralph.com>
2025-12-23 03:25:21 +0100

    switch build script to Bullseye

--- build.cmd
+++ build.cmd
@@ -1,14 +1,2 @@
 @echo Off
-
-echo %~nx0: Formatting...
-dotnet format --verify-no-changes || goto :error
-
-echo %~nx0: Building...
-dotnet build --configuration Release --nologo || goto :error
-
-echo %~nx0: Testing...
-dotnet test --configuration Release --no-build || goto :error
-
-goto :EOF
-:error
-exit /b %errorlevel%
+dotnet run --project targets --no-launch-profile -- %*
