18a4e32c9750045bf8d5e7f704edf6d177077511
Adam Ralph <adam@adamralph.com>
2026-03-21 17:37:48 +0100

    implement standard exception constructor variants

--- SimpleExec/ExitCodeReadException.cs
+++ SimpleExec/ExitCodeReadException.cs
@@ -15,7 +15,62 @@ public class ExitCodeReadException : ExitCodeException
     /// <param name="exitCode">The exit code of the command.</param>
     /// <param name="standardOutput">The contents of standard output (stdout).</param>
     /// <param name="standardError">The contents of standard error (stderr).</param>
-    public ExitCodeReadException(int exitCode, string standardOutput, string standardError) : base(exitCode) => (StandardOutput, StandardError) = (standardOutput, standardError);
+    public ExitCodeReadException(int exitCode, string standardOutput, string standardError) :
+        base(exitCode, CreateMessage(exitCode, standardOutput, standardError))
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+    }
+
+    /// <summary>
+    /// Constructs an instance of a <see cref="ExitCodeReadException"/>.
+    /// </summary>
+    /// <param name="exitCode">The exit code of the command.</param>
+    /// <param name="standardOutput">The contents of standard output (stdout).</param>
+    /// <param name="standardError">The contents of standard error (stderr).</param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception,
+    /// or a <c>null</c> reference (<c>Nothing</c> in Visual Basic) if no inner exception is specified.
+    /// </param>
+    public ExitCodeReadException(int exitCode, string standardOutput, string standardError, Exception innerException) :
+        base(exitCode, CreateMessage(exitCode, standardOutput, standardError), innerException)
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+    }
+
+    /// <summary>
+    /// Constructs an instance of a <see cref="ExitCodeReadException"/>.
+    /// </summary>
+    /// <param name="exitCode">The exit code of the command.</param>
+    /// <param name="standardOutput">The contents of standard output (stdout).</param>
+    /// <param name="standardError">The contents of standard error (stderr).</param>
+    /// <param name="message">The message that describes the error.</param>
+    public ExitCodeReadException(int exitCode, string standardOutput, string standardError, string message) :
+        base(exitCode, message)
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+    }
+
+    /// <summary>
+    /// Constructs an instance of a <see cref="ExitCodeReadException"/>.
+    /// </summary>
+    /// <param name="exitCode">The exit code of the command.</param>
+    /// <param name="standardOutput">The contents of standard output (stdout).</param>
+    /// <param name="standardError">The contents of standard error (stderr).</param>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception,
+    /// or a <c>null</c> reference (<c>Nothing</c> in Visual Basic) if no inner exception is specified.
+    /// </param>
+    public ExitCodeReadException(
+        int exitCode, string standardOutput, string standardError, string message, Exception innerException) :
+        base(exitCode, message, innerException)
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+    }
 
     /// <summary>
     /// Gets the contents of standard output (stdout).
@@ -27,7 +82,13 @@ public class ExitCodeReadException : ExitCodeException
     /// </summary>
     public string StandardError { get; }
 
-    /// <inheritdoc/>
-    public override string Message =>
-        $"{base.Message}{TwoNewLines}Standard output (stdout):{TwoNewLines}{StandardOutput}{TwoNewLines}Standard error (stderr):{TwoNewLines}{StandardError}";
+    /// <summary>
+    /// Create the message that describes the error.
+    /// </summary>
+    /// <param name="exitCode">The exit code of the command.</param>
+    /// <param name="standardOutput">The contents of standard output (stdout).</param>
+    /// <param name="standardError">The contents of standard error (stderr).</param>
+    /// <returns>The message that describes the error</returns>
+    protected static string CreateMessage(int exitCode, string standardOutput, string standardError) =>
+        $"{CreateMessage(exitCode)}{TwoNewLines}Standard output (stdout):{TwoNewLines}{standardOutput}{TwoNewLines}Standard error (stderr):{TwoNewLines}{standardError}";
 }
